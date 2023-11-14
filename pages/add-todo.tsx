1f9a190e58c44ef34e0ae767200056a060d6690b
hashir-00 <hashirhalaldeen00@gmail.com>
2023-11-14 23:53:26 +0530

    added functionality

---
+++ pages/add-todo.tsx
@@ -0,0 +1,103 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { doc } from "@firebase/firestore"; // for creating a pointer to our Document
+import { setDoc } from "firebase/firestore"; // for adding the Document to Collection
+import { firestore } from "../firebase"; // firestore instance
+const AddTodo: NextPage = () => {
+  const [title, setTitle] = useState<string>(""); // title
+  const [description, setDescription] = useState<string>(""); // description
+  const [error, setError] = useState<string>(""); // error
+  
+  const [message, setMessage] = useState<string>(""); // message
+
+  const addTodo = async () => {
+    
+    if (!title || !description) {
+        // check for any null value
+        return setError("All fields are required");
+      }
+    // get the current timestamp
+    const timestamp: string = Date.now().toString();
+    // create a pointer to our Document
+    const _todo = doc(firestore, `todos/${timestamp}`);
+    // structure the todo data
+    const todoData = {
+      title,
+      description,
+      done: false
+    };
+    try {
+      //add the Document
+      await setDoc(_todo, todoData);
+      //show a success message
+      setMessage("Todo added successfully");
+      //alert the message
+       
+      //reset fields
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      //show an error message
+      setError("An error occurred while adding todo");
+    }
+ };
+
+
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault(); // avoid default behaviour
+
+  };
+
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Add todo</title>
+        <meta name="description" content="Next.js firebase todos app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <h1 className={styles.title}>Add todo</h1>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          {error ? (
+            <div className={styles.formGroup}>
+              <p className={styles.error}>{error}</p>
+            </div>
+          ) : null}
+          {message ? (
+            <div className={styles.formGroup}>
+              <p className={styles.success}>
+                {message}. Proceed to <a href="/">Home</a>
+              </p>
+            </div>
+          ) : null}
+          <div className={styles.formGroup}>
+            <label>Title</label>
+            <input
+              type="text"
+              placeholder="Todo title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Description</label>
+            <textarea
+              placeholder="Todo description"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <button type="submit" onClick={() => addTodo()}>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+  
+};
+
+
+export default AddTodo;
---
+++ pages/index.tsx
@@ -0,0 +1,141 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { firestore } from "../firebase";
+import {
+  collection,
+  QueryDocumentSnapshot,
+  DocumentData,
+  query,
+  where,
+  limit,
+  getDocs,
+} from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import { updateDoc } from "@firebase/firestore";
+import { doc } from "firebase/firestore";
+import { deleteDoc } from "@firebase/firestore";
+
+import { setDoc } from "firebase/firestore"; // for adding the Document to Collection
+
+const Home: NextPage = () => {
+  const todosCollection = collection(firestore, "todos");
+  const [todos, setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+ 
+  
+
+  useEffect(() => {
+    // get the todos
+    getTodos();
+    // reset loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  const getTodos = async () => {
+    // construct a query to get up to 10 undone todos
+    const todosQuery = query(
+      todosCollection,
+      where("done", "==", false),
+      limit(10)
+    );
+    // get the todos
+    const querySnapshot = await getDocs(todosQuery);
+
+    // map through todos adding them to an array
+    const result: QueryDocumentSnapshot<DocumentData>[] = [];
+    querySnapshot.forEach((snapshot) => {
+      result.push(snapshot);
+    });
+    // set it to state
+    setTodos(result);
+  };
+
+  const updateTodo = async (documentId: string) => {
+    // create a pointer to the Document id
+    const _todo = doc(firestore, `todos/${documentId}`);
+    // update the doc by setting done to true
+    await updateDoc(_todo, {
+      done: true,
+    });
+    // retrieve todos
+    getTodos();
+    //update alert
+    alert("Todo updated successfully");
+  };
+
+ 
+
+  const deleteTodo = async (documentId: string) => {
+    // create a pointer to the Document id
+    const _todo = doc(firestore, `todos/${documentId}`);
+    // delete the doc
+    await deleteDoc(_todo);
+    
+   
+
+    // retrieve todos
+    getTodos();
+    //delete alert
+    alert("Todo deleted successfully");
+  
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Todos app</title>
+        <meta name="description" content="Next.js firebase todos app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Todos app</h1>
+        <div className={styles.grid}>
+        <div className={styles.card}>
+           
+              <h2 id="add-todo">
+                Add your Todos  <button><a href="/add-todo">here</a></button>
+              </h2>
+            </div>
+          {
+            
+          loading ? (
+            <div className={styles.card}>
+              <h2>Loading</h2>
+            </div>
+          ) : (
+            todos.map((todo, index) => {
+              return (
+                <div className={styles.card} key={index}>
+                  <h1>{todo.data().title}</h1>
+                  <p>{todo.data().description}</p>
+                  <div className={styles.cardActions}>
+                    <button type="button" onClick={() => deleteTodo(todo.id)}>
+                      Delete
+                    </button>
+                    <button type="button" onClick={() => updateTodo(todo.id)}>
+                      Mark as done
+                    </button>
+                    
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a href="#" rel="noopener noreferrer">
+          Todos app
+        </a>
+      </footer>
+    </div>
+  );
+};
+export default Home;
+function setDescription(value: string): void {
+  throw new Error("Function not implemented.");
+}
+
