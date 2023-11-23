636d8927437037d82ec8c96e47c2039302841eca
hashir-00 <hashirhalaldeen00@gmail.com>
2023-11-23 16:41:21 +0530

    buttons get enabled after cancelling the edit function.re did alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,15 @@ import type { NextPage } from "next";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { firestore, todosCollection } from "./utils/firebase";
 import {
-  collection,
   QueryDocumentSnapshot,
   DocumentData,
   query,
   where,
-  limit,
   getDocs,
 } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import { updateDoc } from "@firebase/firestore";
-import { doc, orderBy, serverTimestamp, setDoc } from "firebase/firestore";
+import { doc } from "firebase/firestore";
 import { deleteDoc } from "@firebase/firestore";
 
 import {
@@ -20,79 +18,37 @@ import {
   Button,
   Checkbox,
   Container,
-  Link,
+  Grid,
   ListItem,
   ListItemText,
-  Paper,
   Snackbar,
-  SnackbarOrigin,
   Stack,
   TextField,
   Typography,
-  createSvgIcon,
-  styled,
 } from "@mui/material";
 import * as React from "react";
-import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import DeleteIcon from "@mui/icons-material/Delete";
-import router, { Router, useRouter } from "next/router";
+import router from "next/router";
 import { DATABASES } from "./constants/databases";
-import { todo } from "node:test";
-
+import { Alert, PlusIcon, TextareaAutosize } from "../styles/style";
 //mui
-
-const PlusIcon = createSvgIcon(
-  // credit: plus icon from https://heroicons.com/
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 4.5v15m7.5-7.5h-15"
-    />
-  </svg>,
-  "Plus"
-);
-
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-  props,
-  ref
-) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 
 //home
 
 const Home: NextPage = () => {
-  const [todos, setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
-  const [did, setDid] = useState<boolean>(false); //edit state
+  const [todos, setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]); //todos mapping
+  const [did, setDid] = useState<boolean>(false); //edit conditional render of edit textfields
   const [title, setTitle] = useState<string>(""); // title
   const [description, setDescription] = useState<string>(""); // description
   const [todoID, setTodoID] = useState<string>(""); // todoID
-
-  const [colorType, setColorType] = useState<"error" | "success">(); // error
-  const [msg, setMsg] = useState<string>(""); // error
+  const [boxColor, setBoxColor] = useState<string>(""); //box color
+  const [colorType, setColorType] = useState<"error" | "success">(); //snackbar color
+  const [msg, setMsg] = useState<string>(""); //snackbar message
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string>(""); // error
-  //alert
-  const [open, setOpen] = React.useState(false);
+  const [disable, setDisable] = useState<boolean>(false);
+  const [open, setOpen] = React.useState(false); //alert
 
-  const handleClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
+  //loading the items
   useEffect(() => {
     // get the todos
 
@@ -104,50 +60,76 @@ const Home: NextPage = () => {
     }, 2000);
   }, []);
 
-  //update todo
-  const editTodo = async (documentId:string) => {
+  //alert related
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-    
-     
-        const _todo = doc(firestore, `${DATABASES.TODO}/${documentId}`);
-    // update the doc by setting done to true
+    setOpen(false);
+  };
+
+  //edit todo
+  const editTodo = async (documentId: string) => {
+    //if empty throw an alert
+    if (!title || !description) {
+      return (
+        setColorType("error"), setMsg("Please fill all fields"), setOpen(true)
+      );
+    }
+
+    const _todo = doc(firestore, `${DATABASES.TODO}/${documentId}`);
+    // update the doc by updating the fields
     updateDoc(_todo, {
-   title: title,
-    description: description
-    
+      title: title,
+      description: description,
     });
+    //set alert color and message
     setColorType("success");
     setMsg("Todo Updated");
-
-    setOpen(true);
+    setDisable(false);
+    setBoxColor(""), setOpen(true);
     setDid(false);
 
     await getTodos();
+    //reset the fields
     setTitle("");
     setDescription("");
-    };
-  
-   
+  };
 
+  //cancel edit
+  const cancelEdit = () => {
+    setMsg("Todo not updated"),
+      setColorType("success"),
+      setDisable(false),
+      setTitle(""),
+      setDescription(""),
+      setOpen(true),
+      setDid(false);
+  };
 
+  //get the todos
   const getTodos = async () => {
     // construct a query to get the undone todos
 
-    const todosQuery = query(todosCollection, where("done", "==", false), );
+    const todosQuery = query(todosCollection, where("done", "==", false));
     // get the todos
     const querySnapshot = await getDocs(todosQuery);
-    
 
     // map through todos adding them to an array
     const result: QueryDocumentSnapshot<DocumentData>[] = [];
     querySnapshot.forEach((snapshot) => {
       result.push(snapshot);
     });
-    
+
     // set it to state
     setTodos(result);
   };
 
+  //update todo
   const updateTodo = async (documentId: string) => {
     // create a pointer to the Document id
     const _todo = doc(firestore, `${DATABASES.TODO}/${documentId}`);
@@ -158,9 +140,9 @@ const Home: NextPage = () => {
     setMsg("Todo marked as done");
     setOpen(true);
     await getTodos();
-
- 
   };
+
+  //delete todo
   const deleteTodo = async (documentId: string) => {
     // create a pointer to the Document id
     const _todo = doc(firestore, `${DATABASES.TODO}/${documentId}`); // delete the doc
@@ -171,11 +153,12 @@ const Home: NextPage = () => {
     await getTodos();
     //delete alert
   };
+  //change todo box color
 
   return (
     <Container sx={{ minHeight: "100vh" }}>
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+        <Alert onClose={handleClose} severity={colorType}>
           {msg}
         </Alert>
       </Snackbar>
@@ -225,36 +208,53 @@ const Home: NextPage = () => {
         </Typography>
       </Box>
       {did && (
-              <Stack
-                direction="column"
-                spacing={1}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  marginTop: "5%",
-                }}
-              >
-                <TextField
-                  label="title"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                ></TextField>
-                <TextField
-                  label="description"
-                  value={description} onChange={(e) => setDescription(e.target.value)}
-                 
-                ></TextField>
-                <Button variant="contained" onClick={() =>editTodo(todoID) }>
-               
-                  Update
-                </Button>
-                <Button variant="contained" color="secondary" onClick={() =>setDid(false) }>
-               
-                  cancel
-                </Button>
-              </Stack>
-            )}
+        <Box>
+          <Stack
+            direction="column"
+            spacing={1}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "5%",
+            }}
+          >
+            <TextField
+              label="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            ></TextField>
+            <TextareaAutosize
+              minRows={3}
+              aria-label="empty textarea"
+              placeholder="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></TextareaAutosize>
+          </Stack>
+          <Stack
+            direction="row"
+            spacing={1}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "1%",
+            }}
+          >
+            <Button variant="contained" onClick={() => editTodo(todoID)}>
+              Update
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => cancelEdit()}
+            >
+              cancel
+            </Button>
+          </Stack>
+        </Box>
+      )}
 
       <Box
         sx={{
@@ -271,62 +271,77 @@ const Home: NextPage = () => {
               p: 1,
             }}
           >
-       
             <LoadingButton loading size="large"></LoadingButton>
           </Box>
         ) : (
           todos.map((todo) => {
-            
             return (
-              <Box
+              <Grid //container
+                container
+                spacing="2"
                 sx={{
-                  display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-                  marginBottom: "1%",
+
+                  p: 1,
+                  margin: 2,
+                  boxShadow: 5,
+                  borderRadius: 3,
                 }}
                 key={todo.id}
               >
-                {" "}
-                <ListItem
-                  sx={{
-                    mt: 3,
-                    boxShadow: 5,
-                    borderRadius: 3,
-                    backgroundColor: "ash",
-                    width: "50%",
-                  }}
+                <Grid //checkbox
+                  item
+                  xs={2}
+                  md={2}
                 >
-                  {" "}
-                  <Checkbox onClick={() => updateTodo(todo.id)}></Checkbox>
+                  <Checkbox
+                    disabled={disable}
+                    onClick={() => updateTodo(todo.id)}
+                  ></Checkbox>
+                </Grid>
+
+                <Grid //data
+                  item
+                  xs={2}
+                  md={2}
+                >
                   <ListItemText
                     primary={todo.data().title}
+                   
                     secondary={todo.data().description}
                   ></ListItemText>
-                  <Stack direction="row" spacing={1}>
-                    <Button
-                      variant="contained"
-                      color="error"
-                      startIcon={<DeleteIcon />}
-                      onClick={() => deleteTodo(todo.id)}
-                    >
-                      Delete
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="success"
-                      onClick={() => {
-                        setDid(true) , setTodoID(todo.id);
-                      }}
-                    >
-                      Edit
-                    </Button>
-                  </Stack>
-                </ListItem>
-              </Box>
-              
+                </Grid>
+
+                <Grid //buttons
+                  item
+                  xs={2}
+                  md={2}
+                >
+                  <Button
+                    disabled={disable}
+                    variant="contained"
+                    color="error"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    Delete
+                  </Button>
+                </Grid>
+                <Grid item xs={2} md={2}>
+                  <Button
+                    disabled={disable}
+                    variant="contained"
+                    color="success"
+                    onClick={() => {
+                      setDid(true), setTodoID(todo.id), setDisable(true);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                </Grid>
+              </Grid>
             );
-            
           })
         )}
       </Box>
